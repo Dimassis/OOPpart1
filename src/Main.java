76cf408ea2a9ad75d331dab0e8c390a5d9d67568
Dimassis <166415819+Dimassis@users.noreply.github.com>
2024-08-15 11:21:12 +0500

    HomeWork

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-
+        Pupils pupils = new Pupils();
+        pupils.descriptionPupil("Harry Potter");
+        pupils.descriptionPupil("Marcus Belby");
+        pupils.comparisonOutsideFaculty("Harry Potter", "Marcus Belby");
+        pupils.comparisonInsideFaculty("Padma Patil", "Zhou Chang");
     }
 }
+
+
+
+
+
+
+
+
+
+
